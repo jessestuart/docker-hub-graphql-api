7d5464195791d9c20eebc32531a15f097c21a55a
Jesse Stuart <hi@jessestuart.com>
2019-07-23 18:10:31 -0400

    [wip]
    Signed-off-by: Jesse Stuart <hi@jessestuart.com>

--- handler.ts
+++ handler.ts
@@ -1,11 +1,16 @@
+import _ from 'lodash'
+
 import { queryReposForUser } from './src/services/docker-hub'
+import log from './src/utils/log'
 
-// import log from './src/utils/log'
-
-export const hello = async (event, context, callback) => {
+export const queryDockerHubTopRepos = async (event, context, callback) => {
   const { topRepos, totalPulls } = await queryReposForUser({
     username: 'jessestuart',
   })
+  log.info(
+    { numRepos: _.size(topRepos) },
+    'Successfully retrieved top repositories for user.',
+  )
   // await Promise.all(topRepos.map(popuplateDynamoForRepo))
 
   const response = {
