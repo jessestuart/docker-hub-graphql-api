7d08e53ecdcf3880a6fe6ea0dc42cdb0401dc5cc
Jesse Stuart <hi@jessestuart.com>
2019-08-19 01:29:44 -0400

    [commit message skipped]

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,46 @@
-import { APIGatewayProxyHandler } from 'aws-lambda'
+import { NowRequest, NowResponse } from '@now/node'
 import { queryTopRepos } from './handlers/DockerHub'
 import { DOCKER_USERNAME } from './utils/constants'
 import log from './utils/log'
 
-if (!DOCKER_USERNAME) {
-  log.fatal('Missing required parameters.')
-  process.exit(1)
-}
+// export default (request: NowRequest, response: NowResponse) => {
+//   const { name = 'World' } = request.query
+//   response.status(200).send(`Hello ${name}!`)
+// }
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
-  return {
-    body: JSON.stringify(
-      {
-        message:
-          'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2,
-    ),
-    statusCode: 200,
-  }
-}
+// export const hello: APIGatewayProxyHandler = async (event, _context) => {
+//   return {
+//     body: JSON.stringify(
+//       {
+//         message:
+//           'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+//         input: event,
+//       },
+//       null,
+//       2,
+//     ),
+//     statusCode: 200,
+//   }
+// }
 
-export const queryDockerHub: APIGatewayProxyHandler = async (
-  _event: unknown,
-  _context: unknown,
-) => {
+export default async (_request: NowRequest, response: NowResponse) => {
+  log.info(_request)
+
   if (!DOCKER_USERNAME) {
     log.fatal('Missing required parameters.')
-    process.exit(1)
+    return response.status(400)
   }
 
   const topRepos = await queryTopRepos(DOCKER_USERNAME!)
 
-  const response = {
-    body: JSON.stringify(topRepos),
-    headers: {
-      'Access-Control-Allow-Credentials': true,
-      'Access-Control-Allow-Origin': '*',
-    },
-    statusCode: 200,
-  }
+  // const rep = {
+  //   body: JSON.stringify(topRepos),
+  //   headers: {
+  //     'Access-Control-Allow-Credentials': true,
+  //     'Access-Control-Allow-Origin': '*',
+  //   },
+  //   statusCode: 200,
+  // }
 
-  return response
+  return response.status(200).send(topRepos)
 }
