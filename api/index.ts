d7dd9d69ad351f29da1a59f8a6002febdbb6348a
Jesse Stuart <hi@jessestuart.com>
2019-08-29 21:54:26 -0400

    chore(telemtry): Add apollo metrics.
    Signed-off-by: Jesse Stuart <hi@jessestuart.com>

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,16 @@
 import { ApolloServer } from 'apollo-server'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 import resolvers from './resolvers'
 import typeDefs from './schema'
 import log from './utils/log'
 
 const server = new ApolloServer({
+  engine: {
+    apiKey: process.env.APOLLO_API_KEY,
+  },
   introspection: true,
   playground: true,
   resolvers,
